1c3dc0b79ca628aa56c194888492dd02fe29dec7
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2021-12-29 09:24:41 +0600

    app-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
 function App() {
+  const [user, setLoginUser] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user && user._id ? (
+              <HomePage setLoginUser={setLoginUser}></HomePage>
+            ) : (
+              <Login setLoginUser={setLoginUser}></Login>
+            )}
+          </Route>
+          <Route exact path="/homepage">
+            <HomePage></HomePage>
+          </Route>
+          <Route exact path="/login">
+            <Login setLoginUser={setLoginUser}></Login>
+          </Route>
+          <Route exact path="/register">
+            <Register></Register>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
